862bd9b0d45ce3ec4d1d03932fb731e134cb2a4c
njambi101 <grace.nganga@student.moringaschool.com>
2026-04-19 18:36:42 +0300

    save

---
+++ array.js
@@ -0,0 +1,15 @@
+/*const teamA = ['alice', 'joan'];
+const teamB = ['charlie', 'lucy'];
+const teamC = ['july'];
+const result = (teamA, teamB, teamC);
+console.log(result)*/
+const favoriteGames = ['Zelda', 'Metroid','goo','boob', 'doo', 'what'];
+const favoriteBands = ['Queen', 'RHCP','hohoh0','three','goop'];
+
+function names (){
+  
+    
+}
+
+//const lifeFavorites = [favoriteGames, favoriteBands];
+//console.log(lifeFavorites)
